3e9734d9046c23ff44626c9ff8d4d4465c92c71b
Payacucha <srr.donoso@yavirac.edu.ec>
2023-07-20 22:33:13 -0500

    first commit

---
+++ src/components/TaskList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container p-5">
+  <ul class="list-group">
+    <li
+      class="list-group-item list-group-item-action p-4"
+      style="cursor: pointer"
+      v-for="(task, index) in tasks"
+      :key="index"
+      @click="this.$router.push(`/tasks/${task.id}`)"
+    >
+      {{ index + 1 }}.
+      {{ task.title }}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script lang="ts">
+import { Task } from "@/interfaces/Task";
+import { getTasks } from "@/services/TaskService";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "tasks-list",
+  data() {
+    return {
+      tasks: [] as Task[],
+    };
+  },
+  methods: {
+    async loadTasks() {
+      try {
+        const res = await getTasks();
+        this.tasks = res.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  mounted() {
+    this.loadTasks();
+  },
+});
+</script>
